dcee84e088257b35eb35f62c6319f484a96a948f
Kitti Solomon <40053238+kittisolomon@users.noreply.github.com>
2023-09-07 19:22:39 +0100

    Update index.php

--- index.php
+++ index.php
@@ -4,15 +4,17 @@ header('Content-Type: application/json');
 
 date_default_timezone_set('UTC');
 
+if(!isset($_GET['slack_name']) && !isset($_GET['track'])){
+
+echo $response = json_encode(['response_code' => 402, 'message' => 'slack_name or track cannot be empty']);
+exit();
+
+}else{
+ 
 $slack_name = $_GET['slack_name'];
-
+   
 $track = $_GET['track'];
-
-if(empty($slack_name) || empty($track)){
-
-   echo $response = json_encode(['response_code' => 402, 'message' => 'slack_name or track cannot be empty']);
-   exit();
-   }
+ 
 $current_day = Date('l');
 
 $utc_time = Date('Y-m-d h:i:s');
@@ -20,9 +22,8 @@ $utc_time = Date('Y-m-d h:i:s');
 $github_file_url = 'https://github.com/kittisolomon/HNG-X-Stage-One-Task/blob/main/index.php';
 
 $github_repo_url = 'https://github.com/kittisolomon/HNG-X-Stage-One-Task';
-
 $response_code = 200;
-
+}
 
 echo $response = json_encode([
     'slack_name' => $slack_name,
